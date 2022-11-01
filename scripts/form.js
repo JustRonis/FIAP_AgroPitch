767b58099bb3095a152b4dcf53fa7d5ecad8480a
justronis <ronaldo.luizcp@gmail.com>
2022-11-01 15:52:30 -0300

    pagina de form ainda não finalizada

--- scripts/form.js
+++ scripts/form.js
@@ -12,20 +12,46 @@ const sucess = document.querySelector("#sucess");
 const errorNodes = document.querySelectorAll(".error");
 
 
-
-
 function validarForm(){
 
     clearMessages();
 
     if(nome.value.length < 1){
         errorNodes[0].innerText = "Nome não pode ser branco";
-        nome.classList.add("error-border")
+        nome.classList.add("error-border");
     }
+
+    if(email.value.lenght < 1){
+        errorNodes[1].innerText = "Campo obrigatório";
+        email.classList.add("error-border");
+    }
+    
+    if(!validateEmail(email.value)){
+        errorNodes[1].innerText = "E-mail inválido";
+        email.classList.add("error-border");
+    }
+
+
+
+    if(!validarCelular(celular.value)){
+        errorNodes[2].innerText = "Celular inválido";
+        email.classList.add("error-border");
+    }
+    
+
+    if(endereco_rua.value.length < 1){
+        errorNodes[3].innerText = "Campo obrigatório";
+        nome.classList.add("error-border");
+    }
+
+   
 }
 
 function clearMessages(){
-    for(let i = 0; i < errorNodes.length, i++){
+    for(let i = 0; i < errorNodes.length, i++;){
         errorNodes[i].innerText = "";
     }
+    nome.classList.remove("error-border");
+    email.classList.remove("error-border");
 }
+
